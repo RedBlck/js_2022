ee878746772f8dff0ad9774557f633ac852995ef
pents <pentsakav@gmail.com>
2022-08-20 12:47:12 +0300

    lesson 6 hw 0.5v

--- lesson6/hw/main.js
+++ lesson6/hw/main.js
@@ -1,19 +1,43 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+// let a = 'hello world';
+// let b = 'lorem ipsum';
+// let c = 'javascript is cool';
+// console.log(a.length, b.length, c.length);
+
+
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+// let a = 'hello world';
+// let b = 'lorem ipsum';
+// let c = 'javascript is cool';
+// console.log(a.toUpperCase(), b.toUpperCase(), c.toUpperCase());
+
+
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-//
-//
+// let a = 'HELLO WORLD';
+// let b = 'LOREM IPSUM';
+// let c = 'JAVASCRIPT IS COOL';
+// console.log(a.toLowerCase(), b.toLowerCase(), c.toLowerCase());
+
+
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-//     let str = 'Ревуть воли як ясла повні';
 // let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
 //
-//
-//
+// let str = 'Ревуть воли як ясла повні';
+// let stringToarray = (str) => str.split(' ');
+// console.log(stringToarray(str));
+
+
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// let arr = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+// console.log(arr.map(value => {
+//     return value + '';
+// }));
+
+
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
