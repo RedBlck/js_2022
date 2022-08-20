ac84922c806b199ee588aba8fcde23dc0ccfa08a
ASUSRedBlack <pentsakav@gmail.com>
2022-08-20 23:19:37 +0300

    lesson 6 hw

--- lesson6/hw/main.js
+++ lesson6/hw/main.js
@@ -40,10 +40,18 @@
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
-// sortNums(nums,'ascending') // [3,11,21]
-// sortNums(nums,'descending') // [21,11,3]
-//
-//
+// let sortNums = (array, direction) => {
+//     if (direction === 'ascending') {
+//         nums.sort((a, b) => a - b)
+//     } else if (direction === 'descending') {
+//         nums.sort((a,b)=>b - a)
+//     }
+//     return array;
+// };
+// console.log(sortNums(nums, 'ascending'));// [3,11,21]
+// console.log(sortNums(nums, 'descending')); // [21,11,3]
+
+
 // - є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
@@ -54,8 +62,13 @@
 //     {title: 'Frontend', monthDuration: 4}
 // ];
 // -- відсортувати його за спаданням за monthDuration
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// console.log(coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration));
 //
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// console.log(coursesAndDurationArray.filter((item) => item.monthDuration > 5));
+
+
+
 // описати колоду карт
 // - знайти піковий туз
 // - всі шістки
@@ -69,3 +82,200 @@
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
+// let cards = [
+//     {
+//         cardSuit: null,
+//         value: 'Joker',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: null,
+//         value: 'Joker',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'ace',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'ace',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'ace',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'ace',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'jack',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'jack',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'jack',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'jack',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'queen',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'queen',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'queen',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'queen',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'king',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'king',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'king',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'king',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '10',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '10',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '10',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '10',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '9',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '9',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '9',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '9',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '8',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '8',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '8',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '8',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '7',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '7',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '7',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '7',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '6',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '6',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '6',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '6',
+//         color: 'red'
+//     }
+// ];
+// console.log(cards.find(value => value.cardSuit === 'spade' && value.value === 'ace'));
+// console.log(cards.filter(value => value.value === '6'));
+// console.log(cards.filter(value => value.color === 'red'));
+// console.log(cards.filter(value => value.cardSuit === 'diamond'));
+// console.log(cards.filter(value => value.cardSuit === 'clubs' && value.value > '8'|| value.value ==='10'&& value.cardSuit==='clubs'));
