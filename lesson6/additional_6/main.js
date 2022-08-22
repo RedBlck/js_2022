9ef086a3e3bac2b9bbb07ffb5c9e806e39701ba4
pents <pentsakav@gmail.com>
2022-08-22 18:16:07 +0300

    lesson 6 hw, additional_6 0.9v

--- lesson6/additional_6/main.js
+++ lesson6/additional_6/main.js
@@ -8,18 +8,15 @@
 // };
 // document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
-
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
 // let str = 'Каждый охотник желает знать';
 // let delete_characters = (str, length) => str.substr(0, length);
 // document.writeln(delete_characters(str, 7)); // Каждый
 
-
 // - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
 //     let str = "HTML JavaScript PHP";
 // let insert_dash = (str) => str.toUpperCase().replaceAll(' ', '-');
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
-
 
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
 // let fnUp = (str) => str[0].toUpperCase()+str.slice(1);
@@ -45,30 +42,52 @@
 // console.log(validName(n3));
 
 //     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
-let arrRandom = () => {
-    let array = [];
-    for (let i = 0; i < 10; i++) {
-        array.push(Math.round(Math.random() * 100));
-    }
-    return array;
-};
-console.log(arrRandom());
+// let arrRandom = () => {
+//     let array = [];
+//     for (let i = 0; i < 10; i++) {
+//         array.push(Math.round(Math.random() * 100));
+//     }
+//     return array;
+// };
+// console.log(arrRandom());
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
-console.log(arrRandom().sort(a, b => a - b));
+// console.log(arrRandom().sort((a, b)=> a-b));
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
-//
+// console.log(arrRandom().filter((item)=> item%2===0 && item !==0));
+
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+// let capitalize = (str) => {
+//     let arr = str.split(' ').map(value => value[0].toUpperCase() + value.slice(1));
+//     let string = arr.join(' ');
+//     return string;
+// };
+// console.log(capitalize('red and black'));;
+
 // - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
 // Протестувати на значеннях
 // someemail@gmail.com
 // someeMAIL@gmail.com
 // someeMAIL@i.ua
 // some.email@gmail.com
-//
 // Примітка
 // Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
-//
-// - є масивlet coursesArray = [
+// let validator = (str) => {
+//     let a = str.indexOf('@');
+//     if (a > 2) {
+//         let arr = str.split('@');
+//         let krapka = arr[1].indexOf('.');
+//         if (krapka <= 1) {
+//             return 'mail dont valid';
+//         }
+//     } else {
+//         return 'mail dont valid';
+//     }
+//     return 'mail valid';
+// };
+// console.log(validator('some.email@gmail.com'));
+
+// - є масив, відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+// let coursesArray = [
 //     {
 //         title: 'JavaScript Complex',
 //         monthDuration: 5,
@@ -137,23 +156,70 @@ console.log(arrRandom().sort(a, b => a - b));
 //         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
 //     }
 // ];
-//
-//
-// відсортувати його в спадаючому порядку за кількістю елементів в полі modules
-//
+// console.log(coursesArray.sort((a, b) => b.modules.length - a.modules.length));
+
 // - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
-//     let symb = "о", str = "Астрономия это наука о небесных объектах";
-// document.writeln(count(str, symb)) // 5
-//
+// let symb = "о";
+// let str = "Астрономия это наука о небесных объектах";
+// let count = (str, stringsearch) => {
+//     let lenght = str.split(`${stringsearch}`).length -1;
+//     return lenght;
+// };
+//     document.writeln(count(str, symb)); // 5
+
 // - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
-//     let str = "Сила тяжести приложена к центру масс тела";
+// let cutString = (str, n) => {
+//     let resolt = str.split(' ').slice(0, n).join(' ');
+//     return resolt;
+// };
+
+
+// let str = "Сила тяжести приложена к центру масс тела";
 // document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
-//
-//
 // -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+let books = [
+    {
+        name: 'Harry Potter',
+        pages: 250,
+        author: 'Rouling',
+        genre: ['Higt fantasy', 'Adventure']
+    },
+    {
+        name: 'The Lord of the Rings',
+        pages: 800,
+        author: 'Tolkien',
+        genre: ['Higt fantasy', 'Adventure']
+    },
+    {
+        name: 'Ugly Love',
+        pages: 180,
+        author: 'Hoover',
+        genre: 'romance'
+    },
+    {
+        name: 'A Song of Ice and Fire',
+        pages: 2000,
+        author: 'Martin',
+        genre: 'Epic fantasy'
+    },
+    {
+        name: 'The Witcher',
+        pages: 600,
+        author: ['Andrzej Sapkowski', 'Leonardo'],
+        genre: 'fantasy'
+    }
+];
+
 // -знайти наібльшу книжку.
+// let pages = books.map(value => value.pages);
+// console.log(Math.max(...pages));
+
 // - знайти книжку/ки з найбільшою кількістю жанрів
 // - знайти книжку/ки з найдовшою назвою
 // - знайти книжку/ки які писали 2 автори
 // - знайти книжку/ки які писав 1 автор
 // - вісортувати книжки по кількості сторінок по зростанню
+
+
+
+
